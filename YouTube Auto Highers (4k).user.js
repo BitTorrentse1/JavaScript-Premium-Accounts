377b3200d7a69e45638c5446bd0b477a69ad4932
BitTorrentse1 <a3655423@drdrb.net>
2014-05-30 14:25:43 +0300

    Update YouTube Auto Highers (4k).user.js

--- YouTube Auto Highers (4k).user.js
+++ YouTube Auto Highers (4k).user.js
@@ -5,8 +5,8 @@
 // @author        Bit Torrentse
 // @license       Bit Torrentse , http://www.absba.org
 // @supportURL    http://www.absba.org/member.php?u=37252
-// @updateURL
-// @downloadURL   
+// @updateURL     https://raw.githubusercontent.com/BitTorrentse1/JavaScript-Premium-Accounts/master/YouTube%20Auto%20Highers%20(4k).meta.js
+// @downloadURL   https://raw.githubusercontent.com/BitTorrentse1/JavaScript-Premium-Accounts/master/YouTube%20Auto%20Highers%20(4k).user.js
 // @include		  *youtube.*/*
 // @version		  1.0
 // @icon	      http://upload.wikimedia.org/wikipedia/commons/e/e8/Logo_Youtube.svg
